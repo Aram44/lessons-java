e85933b97d209fbc84953e875fbb93c9fd6aadc5
Aram <aramkhudaverdyan98@gmail.com>
2025-07-14 08:40:24 +0400

    add 6 and 7 solutions

--- src/leetcode/les6/Solution.java
+++ src/leetcode/les6/Solution.java
@@ -1,37 +1,41 @@
 package leetcode.les6;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     public static void main(String[] args) {
         System.out.println(convert("PAYPALISHIRING", 3));
+        System.out.println(convert("ABCD", 2));
+//        "PINALSIGYAHRPI"
     }
 
     public static String convert(String s, int numRows) {
-        if (numRows < 2) {
-            return s;
+        if (numRows == 1 || s.length() <= numRows) return s;
+
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < Math.min(numRows, s.length()); i++) {
+            rows.add(new StringBuilder());
         }
-        var chars = s.toCharArray();
-        int step = numRows - 2;
-        int size = numRows + step;
-        StringBuilder res = new StringBuilder();
-        for (int j = 1; j < (chars.length / size)+1; j++) {
-            System.out.println("Start");
-            for (int i = 0; i < numRows; i++) {
-                if (i == 0) {
-                    res.append(chars[j * size - 1]);
-                    System.out.println("Res " + chars[j*i] + "i :" + j*i);
-                } else {
-                    if (i == (size - i)){
-                        res.append(chars[j*i]);
-                        System.out.println("Res " + chars[j*i] + "i :" + i*j + " same");
-                    }else {
-                        res.append(chars[j*i]);
-                        res.append(chars[j*(size - i)]);
-                        System.out.println("Res " + chars[j*i] + " " + chars[j*(size - i)]);
-                        System.out.println("i :" + i*j + " last " + j*(size - i));
-                    }
-                }
+
+        int currentRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows.get(currentRow).append(c);
+
+            if (currentRow == 0 || currentRow == numRows - 1) {
+                goingDown = !goingDown;
             }
+
+            currentRow += goingDown ? 1 : -1;
         }
-        return res.toString();
+
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
     }
 }
