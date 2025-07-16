b6173f362f73e98297c41e8ede02bac5721102f7
Aram <aramkhudaverdyan98@gmail.com>
2025-07-16 12:13:02 +0400

    add more tasks

---
+++ src/leetcode/les12/Solution.java
@@ -0,0 +1,41 @@
+package leetcode.les12;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+        System.out.println(threeSum(new int[]{0, 0, 0, 0}));
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                var total = nums[i] + nums[j] + nums[k];
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
+                } else {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(nums[k]);
+                    res.add(list);
+                    j++;
+                    while (nums[j] == nums[j - 1] && j < k) {
+                        j++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
