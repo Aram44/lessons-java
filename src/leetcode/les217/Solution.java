8fc9e6bda23ed13bec126deeb83ae7971968e8d0
Aram <aramkhudaverdyan98@gmail.com>
2025-07-17 11:47:05 +0400

    add simple case

--- src/leetcode/les217/Solution.java
+++ src/leetcode/les217/Solution.java
@@ -1,6 +1,8 @@
 package leetcode.les217;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Solution {
@@ -11,6 +13,19 @@ public class Solution {
     }
 
     public static boolean containsDuplicate(int[] nums) {
-       return Arrays.stream(nums).boxed().collect(Collectors.toSet()).size() != nums.length;
+        return Arrays.stream(nums).boxed().collect(Collectors.toSet()).size() != nums.length;
+    }
+
+    public boolean containsDuplicateSimpl(int[] nums) {
+        Set<Integer> checkNums = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            var num = nums[i];
+            if (checkNums.contains(num)) {
+                return true;
+            } else {
+                checkNums.add(num);
+            }
+        }
+        return false;
     }
 }
