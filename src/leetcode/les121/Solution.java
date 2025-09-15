24916824e01915eb930d95ea5b4fa248f873be2a
Aram <aramkhudaverdyan98@gmail.com>
2025-09-15 21:13:58 +0400

    add new case for 121

--- src/leetcode/les121/Solution.java
+++ src/leetcode/les121/Solution.java
@@ -5,6 +5,9 @@ public class Solution {
         System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
         System.out.println(maxProfit(new int[]{7, 6, 5, 4, 3, 1}));
         System.out.println(maxProfit(new int[]{2, 1, 2, 0, 1}));
+        System.out.println(maxProfit2(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(maxProfit2(new int[]{7, 6, 5, 4, 3, 1}));
+        System.out.println(maxProfit2(new int[]{2, 1, 2, 0, 1}));
     }
 
     public static int maxProfit(int[] prices) {
@@ -16,6 +19,16 @@ public class Solution {
             } else if (prices[i] - buy > profit) {
                 profit = prices[i] - buy;
             }
+        }
+        return profit;
+    }
+
+    public static int maxProfit2(int[] prices) {
+        int profit = 0;
+        int buy = prices[0];
+        for (int sell : prices){
+            profit = Math.max(profit, sell - buy);
+            buy = Math.min(buy, sell);
         }
         return profit;
     }
